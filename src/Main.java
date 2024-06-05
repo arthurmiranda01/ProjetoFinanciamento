6d3c548a3427c7909e71b552542eec7eaf9f9f3b
Arthur de Carvalho Miranda <104659615+arthurmiranda01@users.noreply.github.com>
2024-06-05 10:52:36 -0300

    Scanner and Classes
    Adicionando métodos as classes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Financiamento {
     //Atríbutos
     double valorImovel;
@@ -18,15 +20,43 @@ class Financiamento {
 
     double calcularPagamentoTotal(){
 
+        return this.calcularPagamentoMensal() * this.prazoFinanciamento * 12;
     }
 }
 
-class InterfaceUsuario {
+class interfaceUsuario {
 
+    double pedirValorImovel(){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Qual o valor do seu imóvel?");
+        double valorImovel = leitor.nextDouble();
+        System.out.println("Valor digitado: R$" + valorImovel);
+        return 0;
+    }
+
+    int pedirPrazoFinanciamento(){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Qual o prazo em anos do financiamento?");
+        double prazoFinanciamento = leitor.nextDouble();
+        System.out.println("Valor digitado: " + prazoFinanciamento + "anos");
+        return 0;
+    }
+
+    double pedirTaxaJuros(){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Qual a taxa de juros anual?");
+        double taxaJuros = leitor.nextDouble();
+        System.out.println("Valor digitado: " + taxaJuros + "%");
+        return 0;
+    }
 }
 
 public class Main {
     public static void main(String[] args){
-
+        interfaceUsuario interfaceUsuario = new interfaceUsuario();
+        double taxaJuros = interfaceUsuario.pedirTaxaJuros();
+        int prazoFinanciamentoEmAnos = interfaceUsuario.pedirPrazoFinanciamento();
+        double valorImovel = interfaceUsuario.pedirValorImovel();
+        Financiamento novoFinanciamento = new Financiamento(valorImovel, prazoFinanciamentoEmAnos, taxaJuros);
     }
 }
